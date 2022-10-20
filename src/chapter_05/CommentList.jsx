f1dbc93420a9469fad3198d7a228dc48de35b880
LAPTOP-2QMUBS4B\youm0 <romingoon@gmail.com>
2022-10-20 15:04:20 +0900

    Components, Props 실습

---
+++ src/chapter_05/CommentList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Comment from './comment';
+
+function CommentList(props) {
+  return (
+    <div>
+      <Comment />
+    </div>
+  );
+}
+
+export default CommentList;
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+import Library from './chapter_03/Library';
+import Clock from './chapter_04/Clock';
+import CommentList from './chapter_05/CommentList';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CommentList />
   </React.StrictMode>
 );
